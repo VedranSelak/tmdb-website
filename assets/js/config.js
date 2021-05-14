a741bee767d8989a106b9a4bb1ef33723e45f5f3
VedranSelak <selakvedran@gmail.com>
2021-05-15 01:34:21 +0200

    added basic grid layout

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,3 @@
 export const APIKEY = "";
 export const BASE_URL = 'https://api.themoviedb.org/3/';
+export const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185/"
--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 import {APIKEY, BASE_URL} from  './assets/js/config.js';
 
-fetch(BASE_URL + 'movie/550?api_key=' + APIKEY)
+fetch(BASE_URL + 'movie/popular?api_key=' + APIKEY + "&language=en-US&page=1")
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => console.log(data.results));
